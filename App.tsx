4c28a1270f7ec5d4d0640c4e5ff6d64eba6d8d3e
klapro <kevinlarocquepro@gmail.com>
2026-07-23 21:24:37 -0400

    merged the app selector in app

--- App.tsx
+++ App.tsx
@@ -6,7 +6,14 @@ import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
+import { useAppSelector } from './src/store/hooks';
 import { ImportScreen } from './src/screens/ImportScreen';
+import { PlayerScreen } from './src/screens/PlayerScreen';
+
+function RootScreen() {
+  const mediaUri = useAppSelector(state => state.playback.mediaUri);
+  return mediaUri ? <PlayerScreen /> : <ImportScreen />;
+}
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -16,7 +23,7 @@ function App() {
       <SafeAreaProvider>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <View style={styles.container}>
-          <ImportScreen />
+          <RootScreen />
         </View>
       </SafeAreaProvider>
     </Provider>
